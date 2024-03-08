18939fc9703f42b604c0adbb3a18749467636876
syazah <thesyazah@gmail.com>
2024-03-08 14:54:18 +0530

    Learnt Modules

---
+++ DAY01/02globals.js
@@ -0,0 +1,14 @@
+//GLOBALS
+console.log(__dirname);
+console.log(__filename);
+console.log(require);
+console.log(module);
+console.log(process);
+
+setInterval(() => {
+  console.log("Hello World");
+}, 1000);
+
+setTimeout(() => {
+  console.log("Hello World");
+}, 1000);
---
+++ DAY01/03modules.js
@@ -0,0 +1,6 @@
+const names = require("./04names");
+const sayHi = (name) => {
+  console.log(`Hello There ${name}`);
+};
+sayHi(names.john);
+sayHi(names.peter);
